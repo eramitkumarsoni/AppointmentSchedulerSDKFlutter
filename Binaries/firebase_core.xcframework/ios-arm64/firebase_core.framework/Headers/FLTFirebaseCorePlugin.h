98034addf4835327469e0e2c8538b0e379cb056a
Amit <amit.kumar@gocadmium.com>
2025-12-18 20:10:00 +0530

    Update: Synced frameworks with terbaru Flutter build (removed firebase_core)

--- Binaries/firebase_core.xcframework/ios-arm64/firebase_core.framework/Headers/FLTFirebaseCorePlugin.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:aab8ffec525ce87c5cb9dd56a14f2b59369fbc00ce6a71d208fd9111cf85a409
-size 626
--- Binaries/firebase_core.xcframework/ios-arm64/firebase_core.framework/Headers/dummy.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f792f194b62e1c5d5830e2006e909609f16efe2a37573fe1720f4dd81ecd02db
-size 216
