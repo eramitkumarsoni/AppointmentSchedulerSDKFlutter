886e9eed4cb594d3647af40c3fe7c8b2ef97350d
Amit <amit.kumar@gocadmium.com>
2026-01-09 20:13:49 +0530

    Specify dynamic library type for binary framework compatibility

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "AppointmentSchedulerSDKFlutter",
+            type: .dynamic,
             targets: ["AppointmentSchedulerSDKFlutter"]
         ),
     ],
